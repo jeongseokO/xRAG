da6fe3a13da8569dd4694f7dd95355fd196144fe
Jeongseok Oh <90920643+jeongseokO@users.noreply.github.com>
2025-08-27 22:16:52 +0900

    Train compressor with Llama 3.1

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,3 +1,5 @@
 from .SFR import SFR
-from .xMistral import XMistralForCausalLM,XMistralConfig
-from .xMixtral import XMixtralConfig,XMixtralForCausalLM
+from .xMistral import XMistralForCausalLM, XMistralConfig
+from .xMixtral import XMixtralConfig, XMixtralForCausalLM
+from .xLlama import XLlamaForCausalLM, XLlamaConfig
+from .xrag_model import XRAGModel
